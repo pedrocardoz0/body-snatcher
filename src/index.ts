01311df012750844abac1e2decb5fb3048c05bfc
Pedro <p-cardozo@hotmail.com>
2023-02-12 10:05:12 -0300

    feat(main): including collectIssueBody function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+import core from '@actions/core'
+import github, { getOctokit } from '@actions/github'
+
 async function main() {
-    console.log('Good')
+    const issue_number = core.getInput('issue-id', { required: true })
+    const token = core.getInput('github-token', { required: true })
+
+    const body = await collectIssueBody(token, issue_number)
+    core.setOutput('body', { name: 'hi!', body })
 }
+
+async function collectIssueBody(token: string, issue_id: string) {
+    const issue_number = parseInt(issue_id)
+    const octo = getOctokit(token)
+
+    const { data } = await octo.rest.issues.get({
+        ...github.context.repo,
+        issue_number
+    })
+
+    return data.body
+}
+
+main()
